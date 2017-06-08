daa3d032aa72d92fa3eeecf96718eae07ca1dc67
Fey Ijaware <fshola4real@yahoo.co.uk>
2017-06-08 15:32:37 +0100

    second commit

--- app/src/main/java/com/example/fey_agape/arihairnbeauty/MainActivity.java
+++ app/src/main/java/com/example/fey_agape/arihairnbeauty/MainActivity.java
@@ -5,11 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
     RelativeLayout introMessage;
-    LinearLayout appContent;
+    ScrollView appContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         introMessage = (RelativeLayout) findViewById(R.id.welcome_message_layout);
-        appContent = (LinearLayout) findViewById(R.id.app_content_layout);
+        appContent = (ScrollView) findViewById(R.id.app_content_layout);
     }
 
     public void dismisWelcomeMessageBox(View view) {
