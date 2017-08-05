8fd84d4db9e5db0e02ae491b62a5c858f9ab6720
Fey Ijaware <fshola4real@yahoo.co.uk>
2017-08-05 03:23:04 +0100

    Initiated log in flow

--- app/src/main/java/com/example/fey_agape/arihairnbeauty/LoginActivity.java
+++ app/src/main/java/com/example/fey_agape/arihairnbeauty/LoginActivity.java
@@ -1,13 +1,58 @@
 package com.example.fey_agape.arihairnbeauty;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static int APP_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
+    private void onLogin(final LoginType loginType) {
+        // create intent for the Account Kit activity
+        final Intent intent = new Intent(this, AccountKitActivity.class);
+
+        // configure login type and response type
+        AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(
+                        loginType,
+                        AccountKitActivity.ResponseType.TOKEN
+                );
+        final AccountKitConfiguration configuration = configurationBuilder.build();
+
+        // launch the Account Kit activity
+        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, configuration);
+        startActivityForResult(intent, APP_REQUEST_CODE);
+    }
+
+    public void onPhoneLogin(View view) {
+
+        onLogin(LoginType.PHONE);
+    }
+
+    public void onEmailLogin(View view) {
+
+
+        onLogin(LoginType.EMAIL);
+    }
+
+    private void launchAccountActivity() {
+        Intent intent = new Intent(this, AccountActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
